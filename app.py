a286bcbbaf3b5287244ac63dfab91e7d1e1a7852
unknown <siddharth.sembiyan@gmail.com>
2021-04-23 01:52:14 +0530

    Face Identification image added

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import face_recognition
 import cv2
 dataset="dataset"
 images=os.listdir(dataset)
-img=face_recognition.load_image_file("./grace.jpg")
-#(top,right,bottom,left)=face_recognition.face_locations(img)[0]
-#cv2.rectangle(img,(left,top),(right,bottom),(0,255,0),3)
+img=face_recognition.load_image_file("./author_shelby.jpg")
+(top,right,bottom,left)=face_recognition.face_locations(img)[0]
+cv2.rectangle(img,(left,top),(right,bottom),(0,255,0),5)
 unknow_encode=face_recognition.face_encodings(img)[0]
 for image in images:
     path=os.path.join(image)
@@ -15,9 +15,9 @@ for image in images:
     if result[0]==True:
         name=image.split(".")[0]
         print(name)
-      #  cv2.putText(img,name,(left,top+10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(255,0,0),3)
-       # cv2.imshow("recognize",img)
-        #cv2.waitKey(0)
+        cv2.putText(img,name,(left,top+10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(255,0,0),5)
+        cv2.imshow("recognize",img)
+        cv2.waitKey(0)
         break
     else:
         print("unknow person")
